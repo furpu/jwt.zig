6b1a14796fd2065461d63e8af9ed9eccb4a08639
Furpu <feripuru@proton.me>
2024-08-29 04:42:52 -0300

    feat: JWT decode

--- src/ziwt.zig
+++ src/ziwt.zig
@@ -1,12 +1,13 @@
 const std = @import("std");
 const base64 = std.base64;
 const json = std.json;
+const mem = std.mem;
 
-const Allocator = std.mem.Allocator;
+const Allocator = mem.Allocator;
 
 const Header = struct {
     alg: []const u8,
-    typ: ?[]const u8,
+    typ: ?[]const u8 = null,
 };
 
 pub const Algorithm = enum(u8) {
@@ -18,21 +19,32 @@ pub const Algorithm = enum(u8) {
         "HS256",
     };
 
-    inline fn header(self: Algorithm) Header {
-        var typ: ?[]const u8 = null;
-        if (self != .none) {
-            typ = "JWT";
+    fn parseFromString(s: []const u8) !Algorithm {
+        // TODO: reimplement this in a smarter way.
+        // An actual simple parser should be faster.
+        if (mem.eql(u8, "none", s)) {
+            return .none;
+        } else if (mem.eql(u8, "HS256", s)) {
+            return .hs256;
         }
 
-        return .{
-            .alg = strs[@intFromEnum(self)],
-            .typ = typ,
-        };
+        return error.InvalidOrUnknownAlgorithm;
+    }
+
+    fn header(self: Algorithm) Header {
+        var h = Header{ .alg = strs[@intFromEnum(self)] };
+
+        if (self != .none) {
+            h.typ = "JWT";
+        }
+
+        return h;
     }
 };
 
 pub const Codec = struct {
     _enc: base64.Base64Encoder = base64.Base64Encoder.init(base64.url_safe_no_pad.alphabet_chars, base64.url_safe_no_pad.pad_char),
+    _dec: base64.Base64Decoder = base64.Base64Decoder.init(base64.url_safe_no_pad.alphabet_chars, base64.url_safe_no_pad.pad_char),
     key: []const u8,
 
     pub fn encode(self: Codec, allocator: Allocator, alg: Algorithm, payload: anytype) ![]const u8 {
@@ -50,10 +62,90 @@ pub const Codec = struct {
         return try token.toOwnedSlice();
     }
 
-    pub fn decode(allocator: Allocator, comptime T: type) !T {
-        _ = allocator;
+    const TokenPieces = struct {
+        header: []const u8,
+        payload: []const u8,
+        signature: []const u8,
 
-        return undefined;
+        fn fromString(s: []const u8) !TokenPieces {
+            var self = TokenPieces{
+                .header = undefined,
+                .payload = undefined,
+                .signature = undefined,
+            };
+
+            var parts_iter = std.mem.splitScalar(u8, s, '.');
+
+            if (parts_iter.next()) |part| {
+                self.header = part;
+            } else {
+                return error.InvalidFormat;
+            }
+
+            if (parts_iter.next()) |part| {
+                self.payload = part;
+            } else {
+                return error.InvalidFormat;
+            }
+
+            if (parts_iter.next()) |part| {
+                self.signature = part;
+            } else {
+                return error.InvalidFormat;
+            }
+
+            return self;
+        }
+    };
+
+    pub fn decode(self: Codec, comptime T: type, allocator: Allocator, s: []const u8) !json.Parsed(T) {
+        const token_pieces = try TokenPieces.fromString(s);
+
+        // Decode and parse header so we know which algorithm to use
+        const header_json = try allocator.alloc(u8, try self._dec.calcSizeForSlice(token_pieces.header));
+        defer allocator.free(header_json);
+
+        try self._dec.decode(header_json, token_pieces.header);
+        const parsed_header = try json.parseFromSlice(Header, allocator, header_json, .{});
+        defer parsed_header.deinit();
+
+        // Decode and check signature
+        const alg = try Algorithm.parseFromString(parsed_header.value.alg);
+        switch (alg) {
+            .none => {
+                if (token_pieces.signature.len > 0) {
+                    return error.InvalidSignature;
+                }
+            },
+            .hs256 => {
+                if (token_pieces.signature.len == 0) {
+                    return error.InvalidSignature;
+                }
+
+                const sig_bytes = try allocator.alloc(u8, try self._dec.calcSizeForSlice(token_pieces.signature));
+                defer allocator.free(sig_bytes);
+
+                try self._dec.decode(sig_bytes, token_pieces.signature);
+
+                const content_bytes = s[0..(token_pieces.header.len + token_pieces.payload.len + 1)];
+
+                var computed_sig: [std.crypto.auth.hmac.sha2.HmacSha256.mac_length]u8 = undefined;
+                std.crypto.auth.hmac.sha2.HmacSha256.create(&computed_sig, content_bytes, self.key);
+
+                if (!mem.eql(u8, &computed_sig, sig_bytes)) {
+                    return error.InvalidSignature;
+                }
+            },
+        }
+
+        // Decode and parse the payload
+        const payload_json = try allocator.alloc(u8, try self._dec.calcSizeForSlice(token_pieces.payload));
+        defer allocator.free(payload_json);
+
+        try self._dec.decode(payload_json, token_pieces.payload);
+        const payload = try json.parseFromSlice(T, allocator, payload_json, .{ .allocate = .alloc_always });
+
+        return payload;
     }
 
     fn appendEncoded(self: Codec, bs: []const u8, arr: *std.ArrayList(u8)) !void {
@@ -110,4 +202,22 @@ test "JWT encode with alg=none" {
     try std.testing.expectEqualStrings(expected, enc_str);
 }
 
-test "JWT decode" {}
+test "JWT decode alg=hs256" {
+    const codec = Codec{ .key = test_secret };
+    const enc_str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZXN0X2ZpZWxkIjo1fQ.kwVT7OeKoswn-5rjGKuKr7NUGQx5rAuRA3THFtwqp3Y";
+
+    const payload = try codec.decode(struct { test_field: i32 }, std.testing.allocator, enc_str);
+    defer payload.deinit();
+
+    try std.testing.expectEqual(5, payload.value.test_field);
+}
+
+test "JWT decode alg=none" {
+    const codec = Codec{ .key = test_secret };
+    const enc_str = "eyJhbGciOiJub25lIn0.eyJ0ZXN0X2ZpZWxkIjo1fQ.";
+
+    const payload = try codec.decode(struct { test_field: i32 }, std.testing.allocator, enc_str);
+    defer payload.deinit();
+
+    try std.testing.expectEqual(5, payload.value.test_field);
+}
