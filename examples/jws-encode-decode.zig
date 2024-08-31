cdf010d9cacc64cfd2e89460489ec627a672f709
Furpu <feripuru@proton.me>
2024-08-31 01:48:09 -0300

    chore: format README example to fit GitHub

--- examples/jws-encode-decode.zig
+++ examples/jws-encode-decode.zig
@@ -12,10 +12,14 @@ pub fn main() !void {
     };
 
     // Encode and print the encoded string
-    const jws = try codec.encode(std.heap.page_allocator, ExamplePayload{ .custom_data = "example data" });
+    const payload = ExamplePayload{ .custom_data = "example data" };
+    const jws = try codec.encode(std.heap.page_allocator, payload);
     std.debug.print("JWS = {s}\n", .{jws});
 
     // Decode and show the decoded parts
     const decoded = try codec.decode(ExamplePayload, std.heap.page_allocator, jws);
-    std.debug.print("\nDECODED:\nalg = {s}\ntyp = {?s}\ncustom_data = {s}\n", .{ decoded.header.alg, decoded.header.typ, decoded.payload.custom_data });
+    std.debug.print(
+        "\nDECODED:\nalg = {s}\ntyp = {?s}\ncustom_data = {s}\n",
+        .{ decoded.header.alg, decoded.header.typ, decoded.payload.custom_data },
+    );
 }
